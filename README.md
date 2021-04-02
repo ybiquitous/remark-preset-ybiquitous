3123caa5cfe0edd7cb70cfc00e28f497db76d723
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-04-03 03:44:06 +0900

    feat: add dependencies

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # remark-preset-ybiquitous
 
-This is a personal [remark](https://remark.js.org) preset for [ybiquitous](https://github.com/ybiquitous).
+This package is a personal [remark](https://remark.js.org) preset for [ybiquitous](https://github.com/ybiquitous).
+
+It includes some lint rules, plugins, and so on. See [`index.js`](./index.js) for details.
 
 ## Usage
 
@@ -10,10 +12,20 @@ First, install the package:
 $ npm install remark-preset-ybiquitous
 ```
 
-Then, add it to your remark configuration file (e.g. `.remarkrc.json`):
+Next, add it to your remark configuration (e.g. `.remarkrc.json`):
 
 ```json
 {
   "plugins": ["remark-preset-ybiquitous"]
 }
+```
+
+Finally, run remark:
+
+```console
+$ remark .
+README.md
+  31:1-31:9  warning  Do not use headings with similar content (5:1)  no-duplicate-headings  remark-lint
+
+⚠ 1 warning
 ```
--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 module.exports.plugins = [
+  // Preset
   require("remark-preset-lint-recommended"),
-  require("remark-lint-code-block-syntax"),
-  require("remark-lint-no-heading-punctuation"),
-  require("remark-validate-links"),
 
-  // Avoid conflicts with Prettier
+  // NOTE: Avoid conflicts with Prettier.
   /* eslint-disable node/no-extraneous-require, import/no-extraneous-dependencies -- These packages are included in `remark-preset-lint-recommended` */
   [require("remark-lint-list-item-bullet-indent"), false],
   [require("remark-lint-list-item-indent"), false],
-  /* eslint-enable */
+  /* eslint-enable node/no-extraneous-require, import/no-extraneous-dependencies */
+
+  // Lint rule
+  require("remark-lint-code-block-syntax"),
+  require("remark-lint-file-extension"),
+  require("remark-lint-final-definition"),
+  require("remark-lint-final-newline"),
+  require("remark-lint-first-heading-level"),
+  require("remark-lint-heading-increment"),
+  require("remark-lint-linebreak-style"),
+  require("remark-lint-no-consecutive-blank-lines"),
+  require("remark-lint-no-duplicate-defined-urls"),
+  require("remark-lint-no-duplicate-headings"),
+  require("remark-lint-no-duplicate-headings"),
+  require("remark-lint-no-emphasis-as-heading"),
+  require("remark-lint-no-empty-url"),
+  require("remark-lint-no-file-name-articles"),
+  require("remark-lint-no-file-name-consecutive-dashes"),
+  require("remark-lint-no-file-name-irregular-characters"),
+  require("remark-lint-no-file-name-mixed-case"),
+  require("remark-lint-no-file-name-outer-dashes"),
+  require("remark-lint-no-heading-like-paragraph"),
+  require("remark-lint-no-heading-punctuation"),
+  require("remark-lint-no-missing-blank-lines"),
+  require("remark-lint-no-multiple-toplevel-headings"),
+  require("remark-lint-no-paragraph-content-indent"),
+  require("remark-lint-no-reference-like-url"),
+  require("remark-lint-no-shell-dollars"),
+  require("remark-lint-no-table-indentation"),
+  require("remark-lint-no-tabs"),
+  require("remark-lint-no-unneeded-full-reference-image"),
+  require("remark-lint-no-unneeded-full-reference-link"),
+  require("remark-lint-table-pipes"),
+
+  // Plugin
+  require("remark-frontmatter"),
+  require("remark-gfm"),
+  require("remark-validate-links"),
 ];
